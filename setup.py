306401702a6fe8270d90d8984106d59792db7516
smhty <hussein.ty@gmail.com>
2025-03-14 15:16:39 -0700

    requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import setuptools
 version = "2.4.0"
 
 # Read the contents of README.md
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     readme = fh.read()
 
 # Read the requirements from requirements.txt
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 # Setup function to include the requirements
@@ -19,22 +19,17 @@ setuptools.setup(
     author_email="info@dorna.ai",
     description="Dorna vision package",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://dorna.ai/",
     project_urls={
-        'gitHub': 'https://github.com/dorna-robotics/dorna_vision',
-    },
-    package_data={
-        'dorna_vision': ['model/ocr/*'],
+        "gitHub": "https://github.com/dorna-robotics/dorna_vision",
     },
     packages=setuptools.find_packages(),
+    install_requires=requirements,  # This ensures requirements.txt is used
     classifiers=[
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    license="MIT",
-    include_package_data=True,
-    zip_safe=False,
+    python_requires=">=3.6",
 )
